59bbf7fe25ab261b6a441838d57023789894122b
Joaquín Windmüller <joaquin@rpmsoftware.com>
2015-04-07 15:08:06 -0600

    Introducing cron code to run every day at 8:30am in America/Edmonton timezone

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var configs = require('./config/config');
 var Guardian = require('./lib/guardian').Guardian;
+var CronJob = require('cron').CronJob;
 
 process.env['APP_NAME'] = configs.app.name;
-
-for (var i = configs.subscribers.length - 1; i >= 0; i--) {
-    var config = configs.subscribers[i];
-    var guardian = new Guardian(config, configs.mailer);
-    guardian.runAllTests();
-};
+new CronJob({
+    cronTime: '30 8 * * *',
+    start: true,
+    timeZone: 'America/Edmonton',
+    onTick: function() {
+        for (var i = configs.subscribers.length - 1; i >= 0; i--) {
+            var config = configs.subscribers[i];
+            var guardian = new Guardian(config, configs.mailer);
+            guardian.runAllTests();
+        };
+    }
+});
