1b6b9c7e109e14b126104a3a34e86303ace8baf6
Joaquín Windmuller <joaquin@rpmsoftware.com>
2015-04-08 09:16:26 -0600

    Refactoring code to make cron optional

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var configs = require('./config/config');
+var useEnv = process.env['CG_CONFIG'] != undefined;
+var configs = useEnv ? JSON.parse(process.env['CG_CONFIG']) : require('./config/config');
 var Guardian = require('./lib/guardian').Guardian;
 var CronJob = require('cron').CronJob;
 
 process.env['APP_NAME'] = configs.app.name;
-new CronJob({
-    cronTime: '30 8 * * *',
-    start: true,
-    timeZone: 'America/Edmonton',
-    onTick: function() {
-        for (var i = configs.subscribers.length - 1; i >= 0; i--) {
-            var config = configs.subscribers[i];
-            var guardian = new Guardian(config, configs.mailer);
-            guardian.runAllTests();
-        };
+if (process.env['USE_CRON'] === 'YES') {
+    new CronJob({
+        cronTime: '30 8 * * *',
+        start: true,
+        timeZone: 'America/Edmonton',
+        onTick: runTests
+    });
+} else {
+    runTests();
+}
+
+function runTests() {
+    for (var i = configs.subscribers.length - 1; i >= 0; i--) {
+        var config = configs.subscribers[i];
+        var guardian = new Guardian(config, configs.mailer);
+        guardian.runAllTests();
     }
-});
+}
