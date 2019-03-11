c03d875901ff7f3ed3331627d2c1b19044a1ce96
Joaquín Windmuller <joaquin@windmuller.ca>
2019-03-11 08:03:02 -0600

    Removing scheduling code from JS
    Scheduling should now be handled by the 
    Heroku Scheduler addon
    
    #14

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 var useEnv = process.env['CG_CONFIG'] != undefined;
 var configs = useEnv ? JSON.parse(process.env['CG_CONFIG']) : require('./config/config');
 var CoastGuard = require('./lib/coast-guard').CoastGuard;
-var CronJob = require('cron').CronJob;
 
 process.env['APP_NAME'] = configs.app.name;
 if (!useEnv) {
     // Ignore SSL cert errors
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
-if (process.env['USE_CRON'] === 'YES') {
-    console.log('Running as cron job');
-    new CronJob({
-        cronTime: '0 8 * * *',
-        start: true,
-        timeZone: 'America/Edmonton',
-        onTick: runTests
-    });
-} else {
-    console.log('Running once');
-    runTests();
+if (process.env['STOP'] === 'YES') {
+    return;
 }
+
+console.log('Running once');
+runTests();
 
 function runTests(i) {
     if (i === undefined) {
